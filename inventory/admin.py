616cba9b70218f537690a812f9f0ca082d0c54c6
oyekamal <autocratickamal1@gmail.com>
2024-06-22 13:10:03 +0500

    model added

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Equipment, Booking
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'description',
+        'available',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = ('available', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'equipment',
+        'booking_date',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'user',
+        'equipment',
+        'booking_date',
+        'created_at',
+        'updated_at',
+    )
+    date_hierarchy = 'created_at'
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Equipment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.user.username)
